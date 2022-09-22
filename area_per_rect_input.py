505944d3366a630098182d91018fac8f303bffd0
Krostofer <christopher.di-bert@stu.ocsb.ca>
2022-09-22 13:33:19 +0000

    This program calculates the area and perimeter of a rectangle when given user input.

---
+++ area_per_rect_input.py
@@ -0,0 +1,8 @@
+length = int(input("Enter the length of the rectangle: "))
+width = int(input("Enter the width of the rectangle: "))
+print("")
+area = 2 * (length * width)
+perimeter = 2 * (length + width)
+print(f"The area of the rectangle is {area} cm^2")
+print()
+print(f"The perimeter of the rectangle is {perimeter}cm")
